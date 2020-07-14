1e2b31858c595a7fc56d696a4bd09e9e0504d1f9
Ben Dechrai <ben@dechrai.com>
2020-07-14 14:18:28 +1000

    Added stripe code

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 require("dotenv").config()
 
 var express = require("express")
-var cors = require("cors")
 var app = express()
+
+var cors = require("cors")
 app.use(cors())
+
+var bodyParser = require("body-parser")
+app.use(bodyParser.json())
+
 var jwt = require("express-jwt")
 var jwks = require("jwks-rsa")
+
+var stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+var request = require("request")
 
 var port = process.env.PORT || 3000
 
@@ -49,17 +57,108 @@ const getManagementApiJwt = () => {
   })
 }
 
-app.get("/user/manifestStripeCustomer", jwtCheck, function (req, res) {
-  var request = require("request")
+app.post("/checkout/create", jwtCheck, function (req, res) {
+  getManagementApiJwt().then(data => {
+    const token = data.access_token
+    console.log(token)
+    var options = {
+      method: "GET",
+      url: "https://" + process.env.AUTH0_DOMAIN + "/api/v2/users/" + req.user.sub,
+      headers: { authorization: "Bearer " + token, "content-type": "application/json" },
+      json: true,
+    }
 
+    request(options, async function (error, response, body) {
+      if (error) throw new Error(error)
+      const customerId = body.app_metadata.stripeCustomerId
+      // console.log([customerId, req.body.stripePricingPlanId])
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: [
+          {
+            price: req.body.stripePricingPlanId,
+            quantity: 1,
+          },
+        ],
+        customer: customerId,
+        mode: "subscription",
+        success_url: process.env.HOIST_WEBSITE + "/account",
+        cancel_url: process.env.HOIST_WEBSITE + "/account",
+      })
+
+      res.json(session)
+    })
+  })
+})
+app.post("/portal/create", jwtCheck, function (req, res) {
+  getManagementApiJwt().then(data => {
+    const token = data.access_token
+    console.log(token)
+    var options = {
+      method: "GET",
+      url: "https://" + process.env.AUTH0_DOMAIN + "/api/v2/users/" + req.user.sub,
+      headers: { authorization: "Bearer " + token, "content-type": "application/json" },
+      json: true,
+    }
+
+    request(options, async function (error, response, body) {
+      if (error) throw new Error(error)
+      const customerId = body.app_metadata.stripeCustomerId
+
+      const session = await stripe.billingPortal.sessions.create({
+        customer: customerId,
+        return_url: process.env.HOIST_WEBSITE + "/account",
+      })
+
+      res.json(session)
+    })
+  })
+})
+
+app.get("/user/getSubscriptions", jwtCheck, function (req, res) {
+  getManagementApiJwt().then(data => {
+    const token = data.access_token
+    console.log(token)
+    var options = {
+      method: "GET",
+      url: "https://" + process.env.AUTH0_DOMAIN + "/api/v2/users/" + req.user.sub,
+      headers: { authorization: "Bearer " + token, "content-type": "application/json" },
+      json: true,
+    }
+
+    request(options, async function (error, response, body) {
+      if (error) throw new Error(error)
+      const customerId = body.app_metadata.stripeCustomerId
+
+      const subscriptions = await stripe.subscriptions.list({
+        customer: customerId,
+        expand: ["data.items.data.price"],
+      })
+
+      res.json(subscriptions)
+    })
+  })
+})
+
+app.get("/user/manifestStripeCustomer", jwtCheck, function (req, res) {
   getManagementApiJwt()
-    .then(data => {
+    .then(async data => {
       const token = data.access_token
+
+      // Create Stripe Customer
+      const customer = await stripe.customers.create({
+        metadata: {
+          auth0sub: req.user.sub,
+        },
+      })
+
+      // Save Stripe Customer ID to App Metadata
       var options = {
         method: "PATCH",
         url: "https://" + process.env.AUTH0_DOMAIN + "/api/v2/users/" + req.user.sub,
         headers: { authorization: "Bearer " + token, "content-type": "application/json" },
-        body: { app_metadata: { stripeCustomerId: req.user.sub } }, // This will be the Stripe Customer ID
+        body: { app_metadata: { stripeCustomerId: customer.id } },
         json: true,
       }
 
